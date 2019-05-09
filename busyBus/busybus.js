efbe18b8ba63615c77f98eca5f2293af107325ce
Daisy <43765859+dzgan@users.noreply.github.com>
2019-05-09 17:01:29 -0400

    Add files via upload

---
+++ busyBus/busybus.js
@@ -0,0 +1,49 @@
+//every 20 mins
+var RandolphHarborDr={
+Direction: Eastbound,
+Line: 60
+}
+//every 10 mins
+var NavyPier={
+Direction: Eastbound,
+Line: 124
+}
+//every 16 mins
+var DevonClark={
+Direction: Northbound,
+Line: 151
+}
+//every 15 mins
+var $103rdStonyIsland={
+Direction: Southbound,
+Line: J14
+}
+//every 10 mins
+var NavyPier={
+Direction: Southbound,
+Line: 124
+}
+
+//Frequency n/a
+var JacksonParkExpress={
+Direction: Reroute,
+Line: 6X
+}
+
+//OUT OF SERVICE
+var CottageGrove={
+//resumes 12:10 am
+Line: 4
+}
+//OUT OF SERVICE
+var Madison={
+//resumes 12:10 am
+Line: 20
+}
+//OUT OF SERVICE
+var StreetervilleTaylor={
+//resumes 6:00 am
+Line: 157
+}
+
+setInterval
